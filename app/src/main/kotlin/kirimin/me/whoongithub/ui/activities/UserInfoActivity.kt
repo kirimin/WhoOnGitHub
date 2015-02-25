e7bbb9b3c7c7c95b4a4fa7287ee0be2ff1a15ede
kirimin <cc@kirimin.me>
2015-02-25 22:14:40 +0900

    no message

---
+++ app/src/main/kotlin/kirimin/me/whoongithub/ui/activities/UserInfoActivity.kt
@@ -0,0 +1,35 @@
+package kirimin.me.whoongithub.ui.activities
+
+import android.os.Bundle
+import android.support.v7.app.ActionBarActivity
+import kirimin.me.whoongithub.R
+import kirimin.me.whoongithub.models.User
+import android.util.Log
+import kirimin.me.whoongithub.network.apis.UsersApi
+import kirimin.me.whoongithub.network.RequestQueueSingleton
+import rx.schedulers.Schedulers
+import rx.android.schedulers.AndroidSchedulers
+import android.widget.TextView
+import android.widget.ImageView
+import com.squareup.picasso.Picasso
+
+public class UserInfoActivity : ActionBarActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_info)
+        val userInfoText = findViewById(R.id.userInfoNameText) as TextView
+        val idText = findViewById(R.id.userInfoIdText) as TextView
+        val iconImage = findViewById(R.id.userInfoIconImage) as ImageView
+        UsersApi.request(RequestQueueSingleton.get(getApplicationContext()), "kirimin")
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        { user ->
+                            userInfoText.setText(user.name)
+                            idText.setText(user.login)
+                            Picasso.with(this).load(user.avatarUrl).into(iconImage)
+                        },
+                        { e -> e.printStackTrace() })
+    }
+}
